5bc062da4742868ae4d5630c7340b45faee22912
brn2bwild <brn_2_b_wild@hotmail.com>
2023-09-05 22:22:17 -0600

    Cambios realizados para el deploy

--- resources/views/reservaciones/crear.blade.php
+++ resources/views/reservaciones/crear.blade.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Reservaciones La Tiendita VNV</title>
-  <link href="{{asset('css/app.css')}}" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Reservaciones La Tiendita VNV</title>
+	<link href="{{asset('css/app.css')}}" rel="stylesheet">
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="{{asset('js/app.js')}}"></script>
-  <script src="{{asset('js/sweetalert2@11.js')}}"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="{{asset('js/app.js')}}"></script>
+	<script src="{{asset('js/sweetalert2@11.js')}}"></script>
 
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
-  @livewireStyles
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+	@livewireStyles
 </head>
-<body class="antialiased" style="background-color: #F2D1C9">
-  <div class="container mx-auto" style="background-color: #F2D1C9">
-    <div class="flex justify-center px-6 my-12">
-      <livewire:reservaciones/>
-    </div>
-  </div>
-  <footer class="text-center text-white">
-    <div class="flex justify-center" style="background-color: rgba(0, 0, 0, 0.05);">
-      <span class="mr-2">© 2022 Copyright:</span>
-      <a class="text-white hover:text-black" href="https://github.com/Devildrack">DevilSoft</a>
-    </div>
-  </footer>
 
-  @livewireScripts
-  <script>
-    window.addEventListener('swal:confirmar', event => {
+<body class="antialiased" style="background-color: #F2D1C9">
+	<div class="container mx-auto" style="background-color: #F2D1C9">
+		<div class="flex justify-center px-6 my-12">
+			<livewire:reservaciones />
+		</div>
+	</div>
+	<footer class="text-center text-white">
+		<div class="flex justify-center" style="background-color: rgba(0, 0, 0, 0.05);">
+			<span class="mr-2">La Tiendita VNV Todos los derechos reservados © 2023.</span>
+			<a class="text-white hover:text-black" href="https://github.com/Devildrack">Devildrack</a>
+			<a class="text-white hover:text-black ml-4" href="https://github.com/Devildrack">brn2bwild</a>
+
+		</div>
+	</footer>
+
+	@livewireScripts
+	<script>
+		window.addEventListener('swal:confirmar', event => {
       Swal.fire({
         title: event.detail.title,
         text: event.detail.text,
@@ -55,10 +59,10 @@
         timer: 2500
       })
     })
-  </script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
-  <script>
-    $(document).ready(function(){
+	</script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+	<script>
+		$(document).ready(function(){
       $('.timepicker').timepicker({
         timeFormat: 'h:mm p',
         interval: 30,
@@ -75,9 +79,7 @@
         }
       })
     })
-  </script>
+	</script>
 </body>
+
 </html>
-
-
-
